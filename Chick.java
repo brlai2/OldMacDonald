7666f250486de0ff319130de8212945f32a0e5b5
brlai2 <31224382+brlai2@users.noreply.github.com>
2017-10-24 07:57:02 -0700

    10.24.17

--- Chick.java
+++ Chick.java
@@ -1,33 +1,36 @@
 //Chick bob = new Chick();
 class Chick implements Animal
 {     
-     private String setType;     
-     private String setSound;      
-     public Cow()(String myType,String mySound)
+     private String myType;     
+     private String mySound;      
+     public Chick(String type,String sound)
      {         
          myType = type;         
          mySound = sound;     
      }     
-     public Chick()     {         
-         myType = "unknown";         
-         mySound = "unknown";     
+     public Chick(String type, String sound, String sound2)     {         
+         myType = type;
+         if(Math.random()*1>0.5)
+          mySound = sound
+         else
+          mySound = sound2;     
      }      
      public String getSound(){return getSound;}     
      public String getType(){return getType;}
-     public int getType()
-     {
-       return myType;
-     }
-     public int getSound()
-     {
-       return mySound;
-     }
-     public setType(String type)
-     {
-       myType = String setType;
-     }
-     public setSound(String set)
-     {
-       mySound = String setSound;
-     }
+//      public int getType()
+//      {
+//        return myType;
+//      }
+//      public int getSound()
+//      {
+//        return mySound;
+//      }
+//      public setType(String type)
+//      {
+//        myType = String setType;
+//      }
+//      public setSound(String set)
+//      {
+//        mySound = String setSound;
+//      }
 }
