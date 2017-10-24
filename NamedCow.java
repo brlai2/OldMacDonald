eca47ea7b43217f9075fd282bd5a121605e8a68f
brlai2 <31224382+brlai2@users.noreply.github.com>
2017-10-24 08:14:32 -0700

    10.24.17

---
+++ NamedCow.java
@@ -0,0 +1,14 @@
+class NamedCow extendes Cow
+{
+  private String myName;
+  public NamedCow(String type, String name, String sound)
+  {
+    myType = type;
+    myName = name;
+    mySound = sound;
+  }
+  public String getName()
+  {
+    return myName;
+  }
+}
